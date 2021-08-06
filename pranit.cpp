cc305877f26d0070e44319b47a48dc3a83355274
Pranit Patil <dev.pranitpatil@gmail.com>
2021-08-06 22:55:36 +0530

    backtracking rat in maze done

--- pranit.cpp
+++ pranit.cpp
@@ -63,12 +63,72 @@ int friendsPairing(int n){
     return friendsPairing(n-1)+friendsPairing(n-2)*(n-1);
 }
 
+// Backtracking
+
+bool isSafe(int** arr, int x, int y, int n){
+    if(x<n && y<n && arr[x][y]==1){
+        return true;
+    }
+    return false;
+}
+
+bool ratInMaze(int** arr, int x, int y, int n, int** solutionArr){
+
+    if(x==n-1 && y==n-1){
+        solutionArr[x][y]=1;
+        return true;
+    }
+
+    if(isSafe(arr, x, y, n)){
+        solutionArr[x][y]=1;
+        if(ratInMaze(arr, x+1, y, n, solutionArr)){ //right shift
+            return true;
+        }
+        if(ratInMaze(arr, x, y+1, n, solutionArr)){ //bottom shift
+            return true;
+        }
+        solutionArr[x][y]=0;  //Backtrack
+        return false;
+    }
+
+    return false;
+}
 
 
 
 int main(){
 
-    cout<<"Pranit";
+    int n;
+    cin>>n;
+
+    int** arr = new int*[n];
+    for (int i=0; i<n;i++){
+        arr[i]=new int [n];
+    }
+
+    for(int i=0; i<n; i++){
+        for (int j=0; j<n; j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int** solutionArr = new int*[n];
+    for (int i=0; i<n;i++){
+        solutionArr[i]=new int [n];
+        for(int j=0; j<n; j++){
+            solutionArr[i][j]=0;
+        }
+    }
+
+    if (ratInMaze(arr,0,0,n,solutionArr)){
+        for(int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                cout<<solutionArr[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+   
     // cout<<friendsPairing(5);
     // cout<<tilingWays(4);
     // cout<<possiblePath(5,5);
